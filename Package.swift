f1f393aa1978f42a3858494a3bf7fe5cebf2d4a4
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-07-22 17:33:19 +0200

    base58 implementation

--- Package.swift
+++ Package.swift
@@ -11,13 +11,18 @@ let package = Package(
             name: "Base58",
             targets: ["Base58"]),
     ],
+	dependencies: [
+		.package(url: "https://github.com/attaswift/BigInt", .upToNextMajor(from: "5.1.0")),
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Base58"),
+            name: "Base58",
+			dependencies: ["BigInt"]
+		),
         .testTarget(
             name: "Base58Tests",
-            dependencies: ["Base58"]),
+            dependencies: ["Base58", "BigInt"]),
     ]
 )
