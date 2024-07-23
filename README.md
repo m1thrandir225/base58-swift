2a19867c8b7c6ef7ac47e36aa3d7fcac81f452a6
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-07-23 23:03:42 +0200

    feat: added README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Base58 - Swift
+
+This repo contains a simple pure swift implementation of the base58 encoding spec. It is inspired by the [go-base-58](https://github.com/jbenet/go-base58).
+
+This is a personal package and probably should not be used. But if anyone wants to use it you can through Swift PM.
+
+```swift
+.package(url: "https://github.com/m1thrandir225/base58-swift", from: "1.0.0")
+```
+
+---
+
+License MIT © [Sebastijan Zindl](https://sebastijanzindl.me)
--- Sources/Base58/Base58.swift
+++ Sources/Base58/Base58.swift
@@ -1,26 +1,19 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import BigInt
 import Foundation
 
-
-
 public enum Base58 {
 	private static let btcAlphabet = [UInt8]("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz".utf8)
-	private static let flickrAlphabet = [UInt8]("123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ".utf8)
 
-	private static let bigZero = BigUInt(0)
-	private static let bigRadix = BigUInt(58)
-
+	//Default encode method uses the btc alphabet
 	public static func Encode(_ bytes: [UInt8]) -> String {
 		return EncodeAlphabet(bytes, btcAlphabet)
 	}
-
+	//Default decode method uses the btc alphabet
 	public static func Decode(_ input: String) -> [UInt8]? {
 		return DecodeAlphabet(input, btcAlphabet)
 	}
 
+	//Encode a string with a different alphabet
 	public static func EncodeAlphabet(_ bytes: [UInt8], _ alphabet: [UInt8]) -> String {
 		let zero = BigUInt(0)
 		let radix = BigUInt(alphabet.count)
@@ -41,11 +34,10 @@ public enum Base58 {
 			answer.append(alphabet.first!)
 		}
 
-		//answer.reverse()
-
 		return String(bytes: answer, encoding: String.Encoding.utf8)!
 	}
 
+	//Decode an encoded string with a different alphabet
 	public static func DecodeAlphabet(_ input: String, _ alphabet: [UInt8]) -> [UInt8]? {
 		let inputArray = [UInt8](input.utf8)
 		let radix = BigUInt(alphabet.count)
